e599a8e3b2971d52eee163bfe99bf54427442fd5
Jason Madigan <mad@redhat.com>
2015-06-08 16:51:57 +0100

    Sample Test

---
+++ README.md
@@ -0,0 +1,17 @@
+Sample Nightwatch.js Integration Test
+-------------------------------------
+
+Tests the `Backbone Hello World` client app, e.g.: 
+
+https://prod-demos-emea.redhat.feedhenry.com/box/srv/1.1/wid/prod-demos-emea/studio/yismku5dviax4vizr7nxvbsq/file/www/index.html?fh_destination_code=studio&url=https://prod-demos-emea-yismku5jrivvkn2ok6n7q2wt-rht-dev.mbaas1.rht.feedhenry.com
+
+
+Prerequisites:
+
+Must have Firefox installed - although other browsers can also be used to test with
+
+To install locally:
+
+	$ npm install
+	$ npm test
+
---
+++ tests/test_hello.js
@@ -0,0 +1,17 @@
+var url = "https://prod-demos-emea.redhat.feedhenry.com/box/srv/1.1/wid/prod-demos-emea/studio/yismku5dviax4vizr7nxvbsq/file/www/index.html?fh_destination_code=studio&url=https://prod-demos-emea-yismku5jrivvkn2ok6n7q2wt-rht-dev.mbaas1.rht.feedhenry.com";
+
+module.exports = {
+  'Test Backbone App': function(client) {
+    client
+      .url(url)
+      .waitForElementVisible('body', 1000)
+      .assert.title('QuickStart - Backbone')
+      .assert.visible('input[type=text]')
+      .setValue('input[type=text]', 'Joe Bloggs')
+      .waitForElementVisible('button', 1000)
+      .click('button')
+      .pause(1000)
+      .assert.containsText('.hello-result', 'Hello Joe Bloggs')
+      .end();
+  }
+};
